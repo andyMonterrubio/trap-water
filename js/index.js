96e5649584c26982494fcb0fb08ab269c1f9bc9e
andyMonterrubio <andy.mont18@gmail.com>
2020-05-21 15:40:50 -0500

    addign graph and loader

--- js/index.js
+++ js/index.js
@@ -7,11 +7,15 @@ function onlyNumberKey(evt) {
 }
 
 $(document).ready(function(){
+    $(".loader").hide();
+    $(".cont-graph").hide();
+    
     $("#form").submit(function(e){
         e.preventDefault();
-        
         let data = $('#data').val();
         if(data != ""){
+            $(".loader").show();
+
             let arr = data.split("").map(Number);
             let total = 0;
             let left_max = 0, right_max = 0
@@ -30,7 +34,38 @@ $(document).ready(function(){
                 }
             }
 
-            $( "div.result" ).html("<p>The amount of water trapped is: <span class='total'>"+ total +"</span></p>");
+            //Create table
+            let rows = Math.max.apply(null, arr);
+            let columns = arr.length;
+            let table = $("#graph");
+
+            for(let x=0; x<rows; x++){
+                var row = $('<tr>').addClass('row');
+                table.append(row);
+            }
+
+            for(let y=0; y<columns; y++){
+                var column = $('<td>');
+                $('.row').append(column);
+            }
+
+            for(let j=0; j<columns; j++){
+            let num = arr[j];
+                for(let a = 0; a<num; a++){
+                $('.row:eq('+a+') td:eq('+j+')').addClass('color')
+                }
+            }
+
+            setTimeout(function(){ 
+                $(".loader").hide();
+                $(".cont-graph").show();
+                $( "div.result" ).html("<p>The amount of water trapped is: <span class='total'>"+ total +"</span></p>");
+
+            }, 1000);
+
+            
+
+            
         } else {
             alert("Please enter an input")
         }
